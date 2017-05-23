78fb8582f3fa7c57520e1415a4dd8b5481121855
Ben Metzger, Brian Fan and Eric Schoettle <ben+brian+eric@>
2017-05-23 16:44:09 -0700

    not be stable right now - eric

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,20 +1,24 @@
 var Weather = require('./../js/weather.js').weatherModule;
 
-var displayWeather = function(city, weatherData) {
-  $('.showWeather').append(
-    '<p> The humidity in ' + city + ' is ' + weatherData[0] + '% </p>' +
-    '<p> The temperature of ' + city + ' is ' + weatherData[1] + ' Kelvins </p>' +
-    '<p> The temperature of ' + city + ' is ' + weatherData[2] + '°C </p>' +
-    '<p> The temperature of ' + city + ' is ' + weatherData[3] + '°F </p>'
+var displayWeather = function(city, weatherData, number) {
+  $('#showWeather' + number).append(
+    '<h3>'  + city.charAt(0).toUpperCase() + city.slice(1) + '</h3>' +
+    '<p> Humidity: ' + weatherData[0] + '% </p>' +
+    '<p> Temp: ' + weatherData[2] + '°C/' + weatherData[3] + '°F</p>'
   );
-
 };
 
 $(document).ready(function() {
-  var currentWeatherObject = new Weather();
-  $('#weather-location').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    currentWeatherObject.getWeather(city, displayWeather);
+  var currentWeatherObject1 = new Weather();
+  $('#weather-location1').click(function() {
+    var city = $('#location1').val();
+    $('#location1').val("");
+    currentWeatherObject1.getWeather(city, displayWeather, 1);
+  });
+  var currentWeatherObject2 = new Weather();
+  $('#weather-location2').click(function() {
+    var city = $('#location2').val();
+    $('#location2').val("");
+    currentWeatherObject2.getWeather(city, displayWeather, 2);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,19 @@
 var apiKey = require('./../.env').apiKey;
 
 Weather = function() {
-  this.getWeather = function(city, displayWeather) {
+  this.getWeather = function(city, displayWeather, number) {
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      console.log(response)
       var humidity = response.main.humidity;
       var kelvins = response.main.temp.toFixed(0);
       var celsius = (kelvins -273.15).toFixed(0);
       var farenheit = (celsius * 9 / 5 + 32).toFixed(0);
-      displayWeather(city, [humidity, kelvins, celsius, farenheit]);
+      displayWeather(city, [humidity, kelvins, celsius, farenheit], number);
     }).fail(function(error) {
       $('.showWeather').text(error.responseJSON.message);
     });
   };
 };
+
+
 
 exports.weatherModule = Weather;
